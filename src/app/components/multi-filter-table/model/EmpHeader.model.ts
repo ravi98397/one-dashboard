3dc5a1015162f904f195e71a999651211f1eba15
ravi98397 <ravi98397@gmail.com>
2024-01-09 13:56:12 +0530

    added some header icons

---
+++ src/app/components/multi-filter-table/model/EmpHeader.model.ts
@@ -0,0 +1,29 @@
+export enum SortEnum {
+    unset = 0,
+    asc = 1,
+    desc = 2
+  }
+  
+  export interface ColumnDetl{
+    col: String
+    show: boolean
+    sort: SortEnum
+  }
+
+export interface EmpHead {
+    id :ColumnDetl
+    Name :ColumnDetl
+    Website :ColumnDetl
+    Rating :ColumnDetl
+    Email :ColumnDetl
+    Phone :ColumnDetl
+    Username :ColumnDetl
+    City :ColumnDetl
+    Country :ColumnDetl
+    Company :ColumnDetl
+    Position :ColumnDetl
+    Updated_on :ColumnDetl
+    Created_on :ColumnDetl
+    Is_admin :ColumnDetl
+    Salary :ColumnDetl
+}
--- src/app/components/multi-filter-table/multi-filter-table.component.ts
+++ src/app/components/multi-filter-table/multi-filter-table.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 import { Employee } from './model/Employee.model';
 import { tbldata } from './model/data';
 import { KeyValuePipe } from '@angular/common';
+import { EmpHead } from './model/EmpHeader.model';
 
 const TBL_DATA: Employee[] = tbldata; 
+
+export enum SortEnum {
+  unset = 0,
+  asc = 1,
+  desc = 2
+}
+
+export interface ColumnDetl{
+  col: String
+  show: boolean
+  sort: SortEnum
+}
 
 @Component({
   selector: 'app-multi-filter-table',
@@ -11,23 +24,23 @@ const TBL_DATA: Employee[] = tbldata;
   styleUrl: './multi-filter-table.component.css'
 })
 export class MultiFilterTableComponent {
-  displayedColumns: string[] = [
-    "id",
-    "Name",
-    "Website",
-    "Rating",
-    "Email",
-    "Phone",
-    "Username",
-    "City",
-    "Country",
-    "Company",
-    "Position",
-    "Updated_on",
-    "Created_on",
-    "Is_admin",
-    "Salary"
-]
+  col_detl: EmpHead = {
+    "id" : { col: "id", show: true, sort: 0 },
+    "Name" : { col: "id", show: true, sort: 0 },
+    "Website" : { col: "id", show: true, sort: 0 },
+    "Rating" : { col: "id", show: true, sort: 0 },
+    "Email" : { col: "id", show: true, sort: 0 },
+    "Phone" : { col: "id", show: true, sort: 0 },
+    "Username" : { col: "id", show: true, sort: 0 },
+    "City" : { col: "id", show: true, sort: 0 },
+    "Country" : { col: "id", show: true, sort: 0 },
+    "Company" : { col: "id", show: true, sort: 0 },
+    "Position" : { col: "id", show: true, sort: 0 },
+    "Updated_on" : { col: "id", show: true, sort: 0 },
+    "Created_on" : { col: "id", show: true, sort: 0 },
+    "Is_admin" : { col: "id", show: true, sort: 0 },
+    "Salary" : { col: "id", show: true, sort: 0 }
+  }
 
   keys : (keyof Employee)[] = ['id',
   'Name',
